b78cbecdc571656b175c479b00ff2c5e892abcda
alliesground <sameer.libang@gmail.com>
2021-08-25 16:35:47 +1000

    onboarding user as stripe customer on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_create :generate_token
+  after_create_commit :onboard_stripe_customer
 
   private
 
@@ -8,5 +9,14 @@ class User < ApplicationRecord
       self.token = SecureRandom.hex
       break unless self.class.exists?(token: token)
     end
+  end
+
+  def onboard_stripe_customer
+    return if is_admin?
+    customer = Stripe::Customer.create({
+      description: "New Customer #{id}"
+    })
+
+    update({ stripe_customer_id: customer.id })
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_041101) do
+ActiveRecord::Schema.define(version: 2021_08_25_055922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_041101) do
     t.boolean "is_admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_customer_id"
   end
 
   add_foreign_key "prices", "products"
