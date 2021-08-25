5776ee0f0b66704a247831e489edb00456ac02a2
alliesground <sameer.libang@gmail.com>
2021-08-25 12:13:26 +1000

    creating stripe products and prices

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,17 @@
 class Price < ApplicationRecord
   belongs_to :product
+
+  after_create_commit :create_stripe_price
+
+  private
+
+  def create_stripe_price
+    if product.stripe_id.present?
+      Stripe::Price.create({
+        unit_amount: base_price_cents,
+        product: product.stripe_id,
+        currency: 'aud'
+      })
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
-  has_many :prices
+  has_many :prices, dependent: :destroy
+
+  after_create_commit :create_stripe_product
+
+  private
+
+  def create_stripe_product
+    stripe_product = Stripe::Product.create({
+      name: name
+    })
+
+    update({stripe_id: stripe_product.id})
+  end
 end
